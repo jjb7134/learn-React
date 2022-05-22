0f2643eb4f1f8f1b82f2bd654c5ab6d4f02ce5c0
firely95 <90025832+jjb7134@users.noreply.github.com>
2022-05-22 16:28:56 +0900

    todoList 복습
    todoList를 다시 만들어보면서 props를 넘겨받는 과정을 공부함

---
+++ todoPrac/TodoForm.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export default function TodoForm(props) {
+    const [input, setInput] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        props.addTodo(input)
+        setInput("")
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="todo-form">
+            <input
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                className="todo-input" 
+                placeholder="할일을 추가 해 보세요" />
+            <button type="submit" className="todo-button">Add Todo</button>
+        </form>
+    )
+}
